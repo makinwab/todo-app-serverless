7d63c85e4d76252c58562d8d0f5b217dd6ff6196
Makinwa Olubukola <makinwa37@gmail.com>
2020-01-25 18:08:45 +0000

    Implement Auth0 authentication (#8)

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-jn1ijedp.eu.auth0.com',            // Auth0 domain
+  clientId: '9WgLWhUsntFM5hqyGlH0qRtS3ltnB1C4',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
