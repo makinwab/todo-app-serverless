d3f6687edba911cbe2aa78a9f45928b31e945405
Makinwa Olubukola <makinwa37@gmail.com>
2020-01-26 01:53:34 +0000

    Implement deleteTodo function (#12)

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -2,10 +2,40 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log(event)
-  // const todoId = event.pathParameters.todoId
+import { createLogger } from '../../utils/logger'
+import * as AWS from 'aws-sdk'
+import { getUserId } from '../utils'
 
-  // TODO: Remove a TODO item by id
-  return undefined
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  createLogger(`Processing delete todo event: ${event}`)
+
+  const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+
+  await deleteTodo(todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  }
+}
+
+async function deleteTodo(todoId: string, userId: string) {
+  try {
+    return await docClient.delete({
+      TableName: todosTable,
+      Key: {
+        todoId,
+        userId
+      }
+    }).promise()
+  } catch(err) {
+    createLogger(`Error while deleting document: ${err}`)
+  }
 }
