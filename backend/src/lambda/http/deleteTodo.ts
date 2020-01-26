7db461d42a884263f6dc6fd6b0fb6e6e3df41c40
Makinwa Olubukola <makinwa37@gmail.com>
2020-01-26 03:43:36 +0100

    Implement generate upload url function

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -3,8 +3,9 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import { createLogger } from '../../utils/logger'
+import { getUserId, todoExists } from '../utils'
+
 import * as AWS from 'aws-sdk'
-import { getUserId } from '../utils'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
@@ -13,6 +14,20 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const todoId = event.pathParameters.todoId
   const userId = getUserId(event)
+  const validTodo = await todoExists(todoId, userId)
+
+  if (!validTodo) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
 
   await deleteTodo(todoId, userId)
 
