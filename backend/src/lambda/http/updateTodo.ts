88582bb2c6c15635dd4dc2714874e79a13d821e8
Makinwa Olubukola <makinwa37@gmail.com>
2020-01-26 01:10:59 +0000

    Implement updateTodos function (#11)

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -2,12 +2,48 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { createLogger } from '../../utils/logger'
+import * as AWS from 'aws-sdk'
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // const todoId = event.pathParameters.todoId
-  // const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-  console.log(event)
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  createLogger(`Processing update todos event: ${event}`)
+
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+  const payload: UpdateTodoRequest = JSON.parse(event.body)
+
+  await updateTodo(todoId, userId, payload)
+
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  }
+}
+
+async function updateTodo(todoId: string, userId: string, payload: UpdateTodoRequest) {
+  await docClient.update({
+    TableName: todosTable,
+    Key: {
+      todoId,
+      userId
+    },
+    ExpressionAttributeNames: {
+      '#N': 'name'
+    },
+    UpdateExpression: 'SET #N = :name, dueDate = :dueDate, done = :done',
+    ExpressionAttributeValues: {
+      ':name': payload.name,
+      ':dueDate': payload.dueDate,
+      ':done': payload.done
+    }
+  }).promise()
 }
